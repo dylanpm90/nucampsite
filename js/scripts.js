57897efaf414e539dde7bc90b48dd6718de7e228
Dylan MacKay <dylan.p.mackay@gmail.com>
2021-08-05 08:20:40 -0500

    updated the js clicks

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(function() {
     $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
+    $("#carouselButton").on('click', function(){
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
@@ -11,10 +11,10 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause"); 
         }
     });
-    $("#reserveButton").click(function(){
+    $("#reserveButton").on("click", function(){
         $('#reserveModal').modal('show');
     });
-    $("#loginButton").click(function(){
+    $("#loginButton").on("click", function(){
         $('#loginModal').modal('show');
     });
 });
